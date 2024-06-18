2032607df9caf5d0886c3fbd4b2d0041a56fc305
Avikal Gupta <avikal.gupta2003@gmail.com>
2024-06-18 17:42:42 +0530

    Intro to the WebApp

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,24 @@
+import HeaderBox from '@/components/ui/HeaderBox';
 import React from 'react'
 
 const Home = () => {
+  const loggedIn= {firstName: "Avikal"};
+
   return (
-    <div> Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account with transactions efficiently"
+
+          />
+          
+        </header>
+      </div>
+    </section>
   )
 }
 
