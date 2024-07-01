c7ba3f2167fe140d6de758c9e42fb5e51b639feb
Avikal Gupta <avikal.gupta2003@gmail.com>
2024-07-01 10:13:34 +0530

    Implemented the layout

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,7 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import Bankcard from './Bankcard'
 
 const RightSidebar = ({user, transactions, banks}:RightSidebarProps) => {
   return (
